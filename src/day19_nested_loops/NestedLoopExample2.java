53d3b2d8f5a8924184fc1a9124697d9ab7375def
Ahmad <ahmad.mammadli97@gmail.com>
2023-10-11 22:01:17 -0400

    c

---
+++ src/day19_nested_loops/NestedLoopExample2.java
@@ -0,0 +1,29 @@
+package day19_nested_loops;
+
+public class NestedLoopExample2 {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Hello World!");
+        }
+        System.out.println("Hello Universe!");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Hello World!");
+        }
+        System.out.println("Hello Universe!");
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Hello World!");
+        }
+        System.out.println("Hello Universe!");
+        System.out.println("***************");
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.println("Hello World!");
+            }
+            System.out.println("Hello Universe!");
+        }
+    }
+}
